255446a74e1bab8044912f3d234f3a0b48d17383
Christine Lin <73617227+christinealin7771@users.noreply.github.com>
2022-10-26 16:19:08 -0400

    Basic Api

--- server/server.py
+++ server/server.py
@@ -1,3 +1,15 @@
-from flask import Flask
+import os
+
+import numpy as np
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
