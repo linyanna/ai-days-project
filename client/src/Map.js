4a68b28167161fb88c50ec8bbdd7a030b1e896fe
Christine Lin <73617227+christinealin7771@users.noreply.github.com>
2022-10-27 00:37:39 -0400

    Heatmap Render

--- client/src/Map.js
+++ client/src/Map.js
@@ -8,9 +8,9 @@ function Map() {
         ()=>{
             let view;
 
-        loadModules(["esri/views/MapView", "esri/WebMap", "esri/layers/GeoJSONLayer"], {
+        loadModules(["esri/views/MapView", "esri/WebMap", "esri/layers/CSVLayer"], {
             cdd:true
-        }).then(([MapView,WebMap, GeoJSONLayer])=>{
+        }).then(([MapView,WebMap, CSVLayer])=>{
             const webmap = new WebMap({
                 basemap: 'topo-vector'
             })
@@ -23,11 +23,35 @@ function Map() {
                 container:MapE1.current
             })
 
-            const geojsonLayer = new GeoJSONLayer({
-                url: "http://opendata.dc.gov/datasets/81a9d9885947483aa2088d81b20bfe66_5.geojson"
+            const renderer = {
+                type: "heatmap",
+                colorStops: [
+                  { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+                  { color: "#472b77", ratio: 0.083 },
+                  { color: "#4e2d87", ratio: 0.166 },
+                  { color: "#563098", ratio: 0.249 },
+                  { color: "#5d32a8", ratio: 0.332 },
+                  { color: "#6735be", ratio: 0.415 },
+                  { color: "#7139d4", ratio: 0.498 },
+                  { color: "#7b3ce9", ratio: 0.581 },
+                  { color: "#853fff", ratio: 0.664 },
+                  { color: "#a46fbf", ratio: 0.747 },
+                  { color: "#c29f80", ratio: 0.83 },
+                  { color: "#e0cf40", ratio: 0.913 },
+                  { color: "#ffff00", ratio: 1 }
+                ],
+                maxDensity: 0.01,
+                minDensity: 0,
+
+                //referenceScale: 10000000,
+              };
+
+            const layer1 = new CSVLayer({
+                url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.csv",
+                renderer: renderer                
             })
 
-            webmap.add(geojsonLayer)
+            webmap.add(layer1)
         })
         
         return()=>{
