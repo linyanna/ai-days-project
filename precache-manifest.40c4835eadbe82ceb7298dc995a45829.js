bd9ca8225d6fc8875ee628b7bcdc53bada97a6ea
Yanna Lin <linyanna@yahoo.com>
2022-10-27 16:21:51 -0400

    Updates

---
+++ precache-manifest.40c4835eadbe82ceb7298dc995a45829.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "98146ca6117f21bf7832",
+    "url": "/ai-days-project/static/css/main.74663b75.chunk.css"
+  },
+  {
+    "revision": "98146ca6117f21bf7832",
+    "url": "/ai-days-project/static/js/main.98146ca6.chunk.js"
+  },
+  {
+    "revision": "558b1b55fb7d8f5ba1e5",
+    "url": "/ai-days-project/static/js/1.558b1b55.chunk.js"
+  },
+  {
+    "revision": "ef4f0e7bca6076f97c61",
+    "url": "/ai-days-project/static/js/runtime~main.ef4f0e7b.js"
+  },
+  {
+    "revision": "8a259ff14c6bccb66021951f97a65ba6",
+    "url": "/ai-days-project/static/media/big_emission.8a259ff1.jpg"
+  },
+  {
+    "revision": "e0ff37f95fa1e68a54276cc89523e27d",
+    "url": "/ai-days-project/static/media/justin.e0ff37f9.jpg"
+  },
+  {
+    "revision": "35672ccd693b1227d9fec08bc1bf0b6b",
+    "url": "/ai-days-project/static/media/christine.35672ccd.jpg"
+  },
+  {
+    "revision": "281f02f1210328aa985db5603235331e",
+    "url": "/ai-days-project/static/media/zoe.281f02f1.jpg"
+  },
+  {
+    "revision": "76b73f6fe063588c8f19bace17023373",
+    "url": "/ai-days-project/static/media/haohui.76b73f6f.jpg"
+  },
+  {
+    "revision": "d775d7bd9e32d7e9d3831d1d95462398",
+    "url": "/ai-days-project/static/media/yanna.d775d7bd.jpg"
+  },
+  {
+    "revision": "f3eafd7592ffadf6199826944798501e",
+    "url": "/ai-days-project/static/media/footershape.f3eafd75.png"
+  },
+  {
+    "revision": "e7b2e186162a606931f3269e842854be",
+    "url": "/ai-days-project/index.html"
+  }
+];
